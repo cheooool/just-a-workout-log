13b765f01202187142a2326a308f10f9e495ad7a
cheooool <g.cheooool@gmail.com>
2022-11-11 22:48:54 +0900

    feat: headers 추가

--- client/src/lib/http-common.ts
+++ client/src/lib/http-common.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import AuthService from '../features/auth/components/services/AuthService';
 
 export default axios.create({
   baseURL: 'http://localhost:4000/api/v1',
   headers: {
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${AuthService.getToken()}`,
   },
 });
