8390297b02c8ae650a0e6df42c712adc642eabb0
cheooool <g.cheooool@gmail.com>
2022-11-13 03:20:17 +0900

    refactor: recoil 적용으로 기존 테스트 로직 제거

--- client/src/pages/Exercises/index.tsx
+++ client/src/pages/Exercises/index.tsx
@@ -1,29 +1,14 @@
-import { useCallback, useState } from 'react';
 import AddExercise from '../../features/exercise/components/AddExercise';
+import EditExercise from '../../features/exercise/components/EditExercise';
 import ExerciseList from '../../features/exercise/components/ExerciseList';
 import PageLayout from '../../layouts/PageLayout';
 
 const Exercises = () => {
-  const [addOpen, setAddOpen] = useState<boolean>(false);
-
-  const handleOpenModal = useCallback(() => {
-    setAddOpen(true);
-  }, []);
-
-  const handleCloseModal = useCallback(() => {
-    setAddOpen(false);
-  }, []);
   return (
     <PageLayout>
-      <button onClick={handleOpenModal}>운동 생성</button>
       <ExerciseList />
-      {addOpen && (
-        <AddExercise
-          open={addOpen}
-          onCancel={handleCloseModal}
-          onCloseModal={handleCloseModal}
-        />
-      )}
+      <AddExercise />
+      <EditExercise />
     </PageLayout>
   );
 };
