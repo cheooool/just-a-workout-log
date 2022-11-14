a29fecf8d043730fd02e74d925a504d3c454e408
cheooool <g.cheooool@gmail.com>
2022-11-14 17:08:16 +0900

    feat: 새 운동 추가 버튼 추가

--- client/src/pages/Exercises/index.tsx
+++ client/src/pages/Exercises/index.tsx
@@ -1,10 +1,38 @@
+import { Button } from 'antd';
+import { useState } from 'react';
+import AddExerciseModal from '../../features/exercise/components/AddExerciseModal';
 import ExerciseList from '../../features/exercise/components/ExerciseList';
 import PageLayout from '../../layouts/PageLayout';
 
 const Exercises = () => {
+  const [isShowing, setIsShowing] = useState<boolean>(false);
+
+  const handleShowingModal = (value: boolean) => {
+    setIsShowing(value);
+  };
+
   return (
-    <PageLayout>
+    <PageLayout
+      pageHeaderProps={{
+        title: '운동 목록',
+        extra: (
+          <Button
+            className="px-0"
+            type="link"
+            onClick={() => handleShowingModal(true)}
+          >
+            새로운 운동 추가
+          </Button>
+        ),
+      }}
+    >
       <ExerciseList />
+      {isShowing && (
+        <AddExerciseModal
+          open={isShowing}
+          onClose={() => handleShowingModal(false)}
+        />
+      )}
     </PageLayout>
   );
 };
