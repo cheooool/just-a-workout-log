9bc775e34c59835724bf6034c725e91b72d8fe0a
cheooool <g.cheooool@gmail.com>
2022-11-14 14:35:47 +0900

    feat: 클라이언트, 서버 state 분리를 위해 react-query 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,13 +5,34 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import ErrorBoundary from './components/ErrorBoundary';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: 1,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <RecoilRoot>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </RecoilRoot>
   </BrowserRouter>
 );
